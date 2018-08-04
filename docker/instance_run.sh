4ac2fb6a02ce57630cc1d789cb84dc3fc3e2bf68
fujinhua <fu.jinhua@zte.com.cn>
2018-08-04 11:16:29 +0800

    Add vfc-db component seed codes
    Change-Id: If1634e4124a71ef0544caef9aee1a08da060a803
    Issue-ID: VFC-954
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ docker/instance_run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function start_redis_server {
+    redis-server &
+}
+
+function start_mysql {
+    service mysql start
+    sleep 1
+}
+
+function create_database {
+    cd /service/vfc/nfvo/db/resources
+    for ms in $(ls)
+    do
+        cd $ms/bin
+        bash initDB.sh root $MYSQL_ROOT_PASSWORD 3306 127.0.0.1
+        cd /service/vfc/nfvo/db/resources
+    done
+    cd /service
+}
+
+function run_forever {
+    while [ ! -f /var/log/mysql.log ]; do
+        sleep 1
+    done
+    tail -F /var/log/mysql.log 
+}
+
+start_redis_server
+start_mysql
+if [ ! -f dbexist.txt ]; then
+    echo 1 > dbexist.txt
+    create_database
+fi 
+run_forever
