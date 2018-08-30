92bd26f3ab183017dde7da0f8d40306aa3caae38
Bharath Thiruveedula <bharath.thiruveedula@verizon.com>
2018-08-30 14:07:28 +0530

    Add SUBSCRIPTION table to GVNFM DB
    Issue-ID: VFC-999
    Change-Id: Ic3cfcfaf09701b363720ccacdf9eaaef3aafd9d8
    Signed-off-by: Bharath Thiruveedula<bharath.thiruveedula@verizon.com>

--- resources/gvnfm/dbscripts/mysql/onap-gvnfm-vnfres-createobj.sql
+++ resources/gvnfm/dbscripts/mysql/onap-gvnfm-vnfres-createobj.sql
@@ -245,6 +245,16 @@ CREATE TABLE `VNF_REG` (
     `PASSWORD` varchar(255) NOT NULL
 )
 ;
-
+CREATE TABLE `SUBSCRIPTION` (
+    `SUBSCRIPTIONID` varchar(200) NOT NULL PRIMARY KEY,
+    `CALLBACKURI` longtext NOT NULL,
+    `AUTHINFO` longtext,
+    `NOTIFICATIONTYPES` longtext,
+    `OPERATIONTYPES` longtext,
+    `OPERATIONSTATES` longtext,
+    `VNFINSTANCEFILTER` longtext,
+    `LINKS` longtext NOT NULL
+)
+;
 COMMIT;
- 
+
