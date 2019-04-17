c81122e86ef85002fc62785c376b0660ceed9432
yangyan <yangyanyj@chinamobile.com>
2019-04-17 16:26:52 +0800

    Fix docker init sh for db
    Issue-ID: VFC-1306
    Change-Id: I14f3ae2665de71d50d8c0be6d4ebccd36d8d77ec
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- docker/instance_run.sh
+++ docker/instance_run.sh
@@ -29,6 +29,8 @@ function run_forever {
 
 start_redis_server
 start_mysql
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e " GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "flush privileges;"
 # if [ ! -f dbexist.txt ]; then
 #     echo 1 > dbexist.txt
 #     create_database
