819265c8f6426927d68dff2d60163d729c01c97a
fujinhua <fu.jinhua@zte.com.cn>
2019-04-19 09:49:22 +0800

    Update docker file for vfc-db
    Change-Id: I66280527a38cc044a947145a64e9ce120c07acaa
    Issue-ID: VFC-1307
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- docker/instance_run.sh
+++ docker/instance_run.sh
@@ -9,17 +9,6 @@ function start_mysql {
     sleep 1
 }
 
-function create_database {
-    cd /service/vfc/nfvo/db/resources
-    for ms in $(ls)
-    do
-        cd $ms/bin
-        bash initDB.sh root $MYSQL_ROOT_PASSWORD 3306 127.0.0.1
-        cd /service/vfc/nfvo/db/resources
-    done
-    cd /service
-}
-
 function run_forever {
     while [ ! -f /var/log/mysql.log ]; do
         sleep 1
@@ -29,10 +18,6 @@ function run_forever {
 
 start_redis_server
 start_mysql
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e " GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "flush privileges;"
-# if [ ! -f dbexist.txt ]; then
-#     echo 1 > dbexist.txt
-#     create_database
-# fi
 run_forever
